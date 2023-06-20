e0e6d2bc394d9b2c0145e8f9d3806bb085c39fd4
MattfromWii-Sports <matthewjosefcruz@gmail.com>
2023-06-20 15:22:37 +0800

    Set Event Listeners For Numbers And Operators, And Set Arrays To Store Them In

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+let numberPreprocess = [];
+let numberFinalProcess = [];
+let operatorFinal = [];
+const numbers = document.querySelectorAll('button.number');
+const operators = document.querySelectorAll('button.operator');
+
+numbers.forEach(number => number.addEventListener('click', function() {
+    numberPreprocess.push(parseInt(this.dataset.value));
+}));
+operators.forEach(operator => operator.addEventListener('click', function() {
+    operatorFinal.push(this.dataset.value);
+    numberFinalProcess.push(parseInt(numberPreprocess.join('')));
+    numberPreprocess = []; //Reset Preprocess Array for next numbers
+}));
 
 //Basic Math operators
 function add(a, b) {
