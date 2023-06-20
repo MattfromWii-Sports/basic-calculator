2538745e39f771cc4e9a97a9a9836efe65f1ecf7
MattfromWii-Sports <matthewjosefcruz@gmail.com>
2023-06-20 22:05:06 +0800

    Add Function That Returns True If The Last Item In finalProcess[] Is An Operator

--- script.js
+++ script.js
@@ -1,17 +1,40 @@
-let numberPreprocess = [];
-let numberFinalProcess = [];
-let operatorFinal = [];
+let preProcess = [];
+let finalProcess = [];
 const numbers = document.querySelectorAll('button.number');
 const operators = document.querySelectorAll('button.operator');
 
-numbers.forEach(number => number.addEventListener('click', function() {
-    numberPreprocess.push(parseInt(this.dataset.value));
-}));
-operators.forEach(operator => operator.addEventListener('click', function() {
-    operatorFinal.push(this.dataset.value);
-    numberFinalProcess.push(parseInt(numberPreprocess.join('')));
-    numberPreprocess = []; //Reset Preprocess Array for next numbers
-}));
+numbers.forEach(number => number.addEventListener('click', numberFunction));
+operators.forEach(operator => operator.addEventListener('click', operatorFunction));
+
+function numberFunction() {
+    preProcess.push(this.dataset.value);
+    console.log(preProcess);
+}
+function operatorFunction() {
+    if (preProcess.length !== 0) {
+        finalProcess.push(parseInt(preProcess.join('')));
+        preProcess = []; //Reset Array for next numbers
+    }
+    let isOperator = operatorCheck();
+    if (isOperator === true) { 
+        finalProcess = finalProcess.slice(0, -1);
+        finalProcess.push(this.dataset.value);  
+    } else {
+        finalProcess.push(this.dataset.value);
+    }
+    console.log(finalProcess);
+}
+
+//Operator Check
+function operatorCheck() {
+    switch (finalProcess[finalProcess.length - 1]) {
+        case '%':
+        case 'x':
+        case '-':
+        case '+':
+            return true
+    }
+}
 
 //Basic Math operators
 function add(a, b) {
