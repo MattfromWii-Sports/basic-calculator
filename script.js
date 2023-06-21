6fdf0d3d1365277c9ca0ace4aa08f4f9917eb10f
MattfromWii-Sports <matthewjosefcruz@gmail.com>
2023-06-21 18:10:19 +0800

    Stop Screen From Shrinking When Empty, And Add Delete Button

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
+let firstNumber = false;
 let preProcess = [];
 let finalProcess = [];
 const currentDisplay = document.getElementById('current-display');
 const lastDisplay = document.getElementById('last-display');
 const resetAC = document.getElementById('reset-button');
+const deleteC = document.getElementById('delete-button');
 const numbers = document.querySelectorAll('button.number');
 const operators = document.querySelectorAll('button.operator');
 
 resetAC.addEventListener('click', resetEverything);
+deleteC.addEventListener('click', deleteLast);
 numbers.forEach(number => number.addEventListener('click', numberFunction));
 operators.forEach(operator => operator.addEventListener('click', operatorFunction));
 
 function numberFunction() {
     if (preProcess.some(item => item === '.') && this.dataset.value === '.') { 
-        return //Avoid multiple '.' after one is in the array
+        return; //Avoid multiple '.' after one is in the array
     }
     preProcess.push(this.dataset.value);
     currentDisplay.textContent = `${finalProcess.join(' ')} ${preProcess.join('')}`;
@@ -25,7 +28,7 @@ function operatorFunction() {
         finalProcess.push(preProcess.join(''));
         preProcess = [];
     }
-    if (finalProcess.length == 0) { 
+    if (finalProcess.length === 0) { 
         return; //Check array to avoid making operators the first item
     }
     let isOperator = operatorCheck();
@@ -37,12 +40,28 @@ function operatorFunction() {
     console.log(finalProcess);
 }
 
-//Reset All
+//Reset & Delete
 function resetEverything() {
     preProcess = [];
     finalProcess = [];
     currentDisplay.textContent = '0';
     lastDisplay.textContent = '0';
+}
+
+function deleteLast() {
+    if (preProcess.length === 0 && finalProcess.length === 0) {
+        return; //Check if the User has typed anything to delete
+    } else if (preProcess.length > 0) { //Last item Number
+        preProcess = preProcess.slice(0, -1);
+    } else if (operatorCheck() === true) { //Last item Operator
+        finalProcess = finalProcess.slice(0, -1); 
+    } else { //Last item Number after deleting an Operator
+        let tempArray = finalProcess[finalProcess.length - 1].split('');
+        tempArray = tempArray.slice(0, -1).join('');
+        finalProcess = finalProcess.slice(0, -1);
+        finalProcess.push(tempArray);
+    }
+    currentDisplay.textContent = `${finalProcess.join(' ')} ${preProcess.join('')}`;
 }
 
 //Operator Check
